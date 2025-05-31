ddc498fe00084ce1c513e4e23ead518d09551092
Mati Joseph <crucialniccur@gmail.com>
2025-05-31 12:25:45 +0300

    add a print and link base to engine to run the commands

--- db/setup.py
+++ db/setup.py
@@ -2,3 +2,6 @@ from sqlalchemy import create_engine
 from models.game import Base
 
 engine = create_engine('sqlite:///games.db')
+
+Base.metadata.create_all(engine)
+print('The database is created with the Tables and Columns innit')
