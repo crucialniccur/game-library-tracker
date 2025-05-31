94aec64eca53b9b7c445295337e2ae115a01f476
Mati Joseph <crucialniccur@gmail.com>
2025-05-31 13:42:05 +0300

    define add_game that adds a game

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from models.game import Game, session
 import argparse
+
+
+def add_game(title, genre, platform):
+    game = Game(title=title, genre=genre, platform=platform)
+    session.add(game)
+    session.commit()
+    print('Game : --{game}-- added succesfully')
