b880c45bf2dd6ffd80ac033b0724834dfbfba970
Mati Joseph <crucialniccur@gmail.com>
2025-05-31 12:13:50 +0300

    define game model with id title, and other properties and define the __tablename__

--- models/game.py
+++ models/game.py
@@ -2,3 +2,13 @@ from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    genre = Column(String)
+    platform = Column(String)
+    completed = Column(Boolean, default=False)
