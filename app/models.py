453ed2d9808e49e97adecc1b4591d7f9f1e69db6
Mati Joseph <crucialniccur@gmail.com>
2025-06-04 09:30:55 +0300

    Refactor  and error handling

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -7,25 +7,18 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
+    username = Column(String, unique=True, nullable=False)
     libraries = relationship('Library', back_populates='user', cascade='all, delete')
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}')>"
 
 class Library(Base):
     __tablename__ = 'libraries'
     id = Column(Integer, primary_key=True)
-    title = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('User', back_populates='libraries')
-    games = relationship('Game', back_populates='library', cascade='all, delete')
-
-class Game(Base):
-    __tablename__ = 'games'
-    id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    genre = Column(String, nullable=False)
-    platform = Column(String, nullable=False)
-    completed = Column(Boolean, default=False)
-    play_time = Column(Integer, default=0)
-    rating = Column(Integer, default=None)
-    library_id = Column(Integer, ForeignKey('libraries.id'))
-    library = relationship('Library', back_populates='games')
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship('User', back_populates='libraries')
+
+    def __repr__(self):
+        return f"<Library(id={self.id}, title='{self.title}', user_id={self.user_id})>"
